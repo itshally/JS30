ea69ca76a005a43c52dc6d88f0e2ff27a4c47a0f
HB <hbianan@gmail.com>
2019-11-04 12:19:31 -0500

    Update main.js

---
+++ 08-Fun_With_HTML5_Canvas/main.js
@@ -0,0 +1,42 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "pink";
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e){
+     // e.preventDefault();
+     if(!isDrawing) return; //stop the function when not running
+     console.log(e)
+
+     ctx.beginPath();
+
+     //where the line starts
+     ctx.moveTo(lastX, lastY);
+
+     //where the line goes to
+     ctx.lineTo(e.offsetX, e.offsetY);
+     ctx.stroke();
+
+     //destructuring an array
+     [lastX, lastY] = [e.offsetX, e.offsetY];
+     // lastX = e.offsetX;
+     // lastY = e.offsetY;
+}
+
+canvas.addEventListener('mousedown', () => {
+     isDrawing = true;
+     [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
